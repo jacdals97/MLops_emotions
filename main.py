b299d55086d81426a44fd19b0f3d7a50fb8b3b82
danielvigild <daniel.vigild@gmail.com>
2024-01-12 14:52:49 +0100

    now updated with better text and extracting the class and probability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from emotions.predict_model import Predictor
+import pandas as pd
 
 # Load the model
 model_name = "distilbert-base-uncased"
@@ -12,13 +13,14 @@ html = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Chat</title>
+        <title>Emotion prediction</title>
     </head>
     <body>
-        <h1>Emotion prediction model</h1>
+        <h1>Emotion prediction service </h1>
+        <h2>Type in the text you wish to predict below, and press "Predict"!</h2>
         <form action="" onsubmit="sendMessage(event)">
             <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
+            <button>Predict</button>
         </form>
         <ul id='messages'>
         </ul>
@@ -53,7 +55,16 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        emotion_class = model.predict(data)
-        await websocket.send_text(f"Message text was: {data} with prediction {emotion_class}")
+        preds = model.predict(data)
+        preds = preds[0] # removes the list thing
+        preds=pd.DataFrame.from_dict(preds,orient='index')
+        preds=preds.T
+        preds=preds.astype({'score': 'float'})
+        preds=preds.round(3)
+        preds.score=preds.score*100
+        await websocket.send_text(f"Message text was: {data} | With predicted label: {preds.label[0]} | And a probability of: {preds.score[0]} %")
 
+
+# run the model using uvicorn main:app --reload
+        
 
